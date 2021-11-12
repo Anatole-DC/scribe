2234ed96e670cf8774eb56f3fb6d645a77db152e
Anatole-DC <adechauveron@gmail.com>
2021-11-13 00:02:05 +0100

    :seedling: Scribe builder base file

---
+++ src/code/scribe_builder.py
@@ -0,0 +1,21 @@
+from typing import Dict, List
+import src.config.settings as settings
+import src.utils.debug as debug
+
+def fetch_subdirectories() -> List[str]:
+    """
+        Fetch subdirectories to generate summary.
+    """
+    pass
+
+def fetch_markdowns() -> List[str]:
+    """
+        Fetch markdowns in subdirectories.
+    """
+    pass
+
+def fetch_doc_elements() -> Dict[str]:
+    """
+        Fetch all the elements in the doc directory.
+    """
+    pass
